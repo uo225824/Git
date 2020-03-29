8a001be0f9152b826f26a3e515933dc57b481874
uo225824 <christiaapin06-gmail.com>
2020-03-29 18:34:33 +0200

    Primer commit

--- Curso Python/nn.py
+++ Curso Python/nn.py
@@ -1,5 +1,6 @@
+import tensorflow as tf
 import numpy as np
-import keras
+from tensorflow import keras
 model=keras.Sequential([keras.layers.Dense(units=1,input_shape=[1])])
 model.compile(optimizer='sgd',loss='mean_squared_error')
 
