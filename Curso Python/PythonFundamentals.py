e36ac2ffd9b484057e16165291ee4b2d42961e09
uo225824 <christiaapin06-gmail.com>
2020-03-29 14:53:15 +0200

    Primer commit

--- Curso Python/PythonFundamentals.py
+++ Curso Python/PythonFundamentals.py
@@ -32,7 +32,65 @@ print(type(1.))
 
 #listas
 
-x=(1,'a',2,'b',3)
+x=[1,'a',2,'b',3]
 print(x)
 x.append(3.3)
 print(x)
+
+
+for i in x:
+    print(i)
+
+i=0
+while( i != len(x) ):
+    print(x[i])
+    i = i + 1
+
+
+[1,2] + [3,4]
+[1]*3
+1 in [1, 2, 3]
+
+x = 'This is a string'
+print(x[0]) #first character
+print(x[0:1]) #first character, but we have explicitly set the end character
+print(x[0:2]) #first two characters
+
+x[-4:-2]
+x[3:]
+
+
+firstname = 'Christopher'
+lastname = 'Brooks'
+
+print(firstname + ' ' + lastname)
+print(firstname*3)
+print('Chris' in firstname)
+
+
+firstname = 'Christopher Arthur Hansen Brooks'.split(' ')[0] # [0] selects the first element of the list
+lastname = 'Christopher Arthur Hansen Brooks'.split(' ')[-1] # [-1] selects the last element of the list
+print(firstname)
+print(lastname)
+
+
+#Diccionarios
+
+print('DICCIONARIOS')
+x = {'Christopher Brooks': 'brooksch@umich.edu', 'Bill Gates': 'billg@microsoft.com'}
+x['Christopher Brooks'] # Retrieve a value by using the indexing operator
+
+for name in x:
+    print(x[name])
+
+for email in x.values():
+    print(email)
+
+for name, email in x.items():
+    print(name)
+    print(email)
+
+x = ('Christopher', 'Brooks', 'brooksch@umich.edu')
+fname, lname, email = x
+
+print(fname)
