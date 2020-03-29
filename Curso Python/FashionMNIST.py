60e8b951a67af40aecbd442a29b1a6dcae71dafb
uo225824 <christiaapin06-gmail.com>
2020-03-29 21:10:09 +0200

    Primer commit

--- Curso Python/FashionMNIST.py
+++ Curso Python/FashionMNIST.py
@@ -16,10 +16,21 @@ print(training_images[0])
 training_images  = training_images / 255.0
 test_images = test_images / 255.0
 
+import tensorflow as tf
+print(tf.__version__)
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('loss')<0.3):
+      print("\nReached 96% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+callbacks = myCallback()
 
 #Model nn
 
 model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu),
                                     tf.keras.layers.Dense(128, activation=tf.nn.relu),
                                     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 
@@ -31,7 +42,7 @@ model.compile(optimizer = tf.optimizers.Adam(),
               loss = 'sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(training_images, training_labels, epochs=5)
+model.fit(training_images, training_labels, epochs=50,callbacks=[callbacks])
 
 
 model.evaluate(test_images, test_labels)
