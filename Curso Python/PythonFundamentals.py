28c00dcac170123d4584113291a02d133d481f1a
uo225824 <christiaapin06-gmail.com>
2020-03-29 18:05:36 +0200

    Primer commit

--- Curso Python/PythonFundamentals.py
+++ Curso Python/PythonFundamentals.py
@@ -94,3 +94,81 @@ x = ('Christopher', 'Brooks', 'brooksch@umich.edu')
 fname, lname, email = x
 
 print(fname)
+
+#Strings
+
+print('Chris'+str(2))
+
+sales_record={
+    'price':3.24,
+    'num_item':4,
+    'person':'Chris'
+}
+
+sales_statement='{} bought {} items at price of {} each for a total of {}'
+
+print(sales_statement.format(sales_record['person'],
+                             sales_record['num_item'],
+                             sales_record['price'],
+                             sales_record['num_item']*sales_record['price']))
+
+
+#Fechas y tiempo
+
+import datetime as dt
+import time as tm
+
+print(tm.time())
+
+dtnow=dt.datetime.fromtimestamp(tm.time())
+print(dtnow)
+
+delta = dt.timedelta(days = 100) # create a timedelta of 100 days
+print(delta)
+
+
+#Objetos y mapas
+
+class Person:
+    department = 'School of Information' #a class variable
+
+    def set_name(self, new_name): #a method
+        self.name = new_name
+    def set_location(self, new_location):
+        self.location = new_location
+
+person = Person()
+person.set_name('Christopher Brooks')
+person.set_location('Ann Arbor, MI, USA')
+print('{} live in {} and works in the department {}'.format(person.name, person.location, person.department))
+
+store1 = [10.00, 11.00, 12.34, 2.34]
+store2 = [9.00, 11.10, 12.34, 2.01]
+cheapest = map(min, store1, store2)
+print(cheapest)
+
+for item in cheapest:
+    print(item)
+
+
+#Funciones lambda y compresion de listas
+
+
+my_function = lambda a, b, c : a + b
+my_function(1, 2, 3)
+
+
+my_list = []
+for number in range(0, 1000):
+    if number % 2 == 0:
+        my_list.append(number)
+my_list
+
+
+my_list = [number for number in range(0,1000) if number % 2 == 0]
+my_list
+
+
+#Numpy
+
+
