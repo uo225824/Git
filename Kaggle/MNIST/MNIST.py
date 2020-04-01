403a9209d7c9d882ca9eac456352dcf4bbb71529
uo225824 <christiaapin06-gmail.com>
2020-04-01 12:46:06 +0200

    Primer commit

--- Kaggle/MNIST/MNIST.py
+++ Kaggle/MNIST/MNIST.py
@@ -7,20 +7,15 @@ import tensorflow as tf
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 
-Y_train = train["label"]
-X_train=train.drop(labels = ["label"],axis = 1)
+Xtrain = train.iloc[:,1:]
+ytrain = train.iloc[:,0]
+print(Xtrain)
 
 # Normalize the data
-X_train=X_train.reshape(60000, 28, 28, 1)
-X_train = X_train / 255.0
-Y_train = Y_train.reshape(10000, 28, 28, 1)
-Y_train=Y_train / 255.0
+Xtrain=Xtrain.values.reshape(-1,28,28,1)
+Xtrain = Xtrain / 255.0
 
-test = test / 255.0
-
-#expand 1 more dimention as 1 for colour channel gray
-X_train = X_train.reshape(X_train.shape[0], 28, 28)
-print(X_train.head())
+test = test.values.reshape(-1,28,28,1) / 255.0
 
 
 model = tf.keras.models.Sequential([
@@ -35,5 +30,14 @@ model = tf.keras.models.Sequential([
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.summary()
-model.fit(X_train, Y_train, epochs=5)
-#test_loss = model.evaluate(test_images, test_labels)
+model.fit(Xtrain, ytrain, epochs=15)
+#test_loss = model.evaluate(test)
+
+
+#Predict result
+result = model.predict(test)
+results = np.argmax(result,axis = 1)
+submission = pd.DataFrame({ 'ImageId' : list(range(1,len(results)+1)),
+             'Label': results})
+
+submission.to_csv('Submission file',index=False,header = True)
