c8dfaf417ab322c861ed2c57164d48c6aaaced36
uo225824 <christiaapin06-gmail.com>
2020-09-13 18:35:56 +0200

    2 commit

--- Tema 4/datadealing.py
+++ Tema 4/datadealing.py
@@ -10,4 +10,44 @@ A,B,C,D
 # to convert the string to unicode:
 # csv_data = unicode(csv_data)
 df = pd.read_csv(StringIO(csv_data))
-df
+
+#Number of NaN
+print(df.isnull().sum())
+
+#Rows without NaN
+df.dropna(axis=0)
+
+#Colums without NaN
+df.dropna(axis=1)
+
+# only drop rows where all columns are NaN
+# (returns the whole array here since we don't
+# have a row with all values NaN)
+df.dropna(how='all')
+
+
+#rows with 4 real values
+df.dropna(thresh=4)
+
+# only drop rows where NaN appear in specific columns (here: 'C')
+df.dropna(subset=['C'])
+
+#Imputing missing values
+#Mean imputation
+
+from sklearn.impute import SimpleImputer
+import numpy as np
+imr = SimpleImputer(missing_values=np.nan, strategy='mean')
+imr = imr.fit(df.values)
+imputed_data = imr.transform(df.values)
+imputed_data
+
+#other method with pandas
+
+a=df.fillna(df.mean())
+
+print(imputed_data)
+print(a)
+
+
+#Categorical data
