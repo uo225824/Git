27409176a452cfaf14376bdba9d0aa62384caa1c
uo225824 <christiaapin06-gmail.com>
2020-09-13 03:13:53 +0200

    firts commit

---
+++ Curso Python/Tema 3/DTreeLearning.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class InformationGain():
+
+
+    def gini(self,p):
+        return p * (1 - p) + (1 - p) * (1 - (1 - p))
+
+
+    def entropy(self,p):
+        return - p * np.log2(p) - (1 - p) * np.log2((1 - p))
+
+
+    def error(self, p):
+        return 1 - np.max([p, 1 - p])
+
+x = np.arange(0.0, 1.0, 0.01)
+IG=InformationGain()
+
+
+ent = [IG.entropy(p) if p != 0 else None for p in x]
+sc_ent = [e * 0.5 if e else None for e in ent]
+err = [IG.error(i) for i in x]
+
+fig = plt.figure()
+ax = plt.subplot(111)
+for i, lab, ls, c, in zip([ent, sc_ent, IG.gini(x), err],
+                          ['Entropy', 'Entropy (scaled)',
+                           'Gini Impurity', 'Misclassification Error'],
+                          ['-', '-', '--', '-.'],
+                          ['black', 'lightgray', 'red', 'green', 'cyan']):
+    line = ax.plot(x, i, label=lab, linestyle=ls, lw=2, color=c)
+
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15),
+          ncol=3, fancybox=True, shadow=False)
+
+ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+plt.ylim([0, 1.1])
+plt.xlabel('p(i=1)')
+plt.ylabel('Impurity Index')
+plt.tight_layout()
+#plt.savefig('./figures/impurity.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+
+#Building a decision tree
+
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import datasets
+
+
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+#Splitting data into 70% training and 30% test data
+from sklearn.model_selection import train_test_split
+from scikit import plot_decision_regions
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=0)
+
+from sklearn.tree import DecisionTreeClassifier
+tree_model = DecisionTreeClassifier(criterion='gini', max_depth=4,random_state=1)
+tree_model.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined,y_combined,classifier=tree_model,test_idx=range(105, 150))
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+
+from sklearn import tree
+tree.plot_tree(tree_model)
+plt.show()
