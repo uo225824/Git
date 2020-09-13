33b4ec498e49b69c67fd1ec7ed852b19dd9e16f2
uo225824 <christiaapin06-gmail.com>
2020-09-13 02:19:53 +0200

    firts commit

--- Curso Python/Tema 3/logisticRegression.py
+++ Curso Python/Tema 3/logisticRegression.py
@@ -45,18 +45,51 @@ sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
 
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
 from sklearn.linear_model import LogisticRegression
 
 lr = LogisticRegression(C=1000.0, random_state=0)
 lr.fit(X_train_std, y_train)
 
 from scikit import plot_decision_regions
+import matplotlib.pyplot as plt
 
-plot_decision_regions(X_combined_std, y_combined,
-                      classifier=lr, test_idx=range(105, 150))
+
+plot_decision_regions(X_combined_std, y_combined,classifier=lr, test_idx=range(105, 150))
 plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 plt.tight_layout()
 # plt.savefig('./figures/logistic_regression.png', dpi=300)
 plt.show()
+
+#Probabilities
+print(lr.predict_proba(X_test_std[:5, :]))
+
+#Regularization
+
+weights, params = [], []
+for c in np.arange(-5, 5):
+    lr = LogisticRegression(C=10.**c, random_state=1, solver='lbfgs', multi_class='ovr')
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10.**c)
+    plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105, 150))
+    plt.xlabel('petal length [standardized]')
+    plt.ylabel('petal width [standardized]')
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+    # plt.savefig('./figures/logistic_regression.png', dpi=300)
+    plt.show()
+
+
+weights = np.array(weights)
+plt.plot(params, weights[:, 0],label='petal length')
+plt.plot(params, weights[:, 1], linestyle='--',label='petal width')
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.legend(loc='upper left')
+plt.xscale('log')
+plt.show()
+
