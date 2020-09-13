b770f077cad528b3994cbdab0708549593956393
uo225824 <christiaapin06-gmail.com>
2020-09-13 03:18:14 +0200

    firts commit

--- Curso Python/Tema 3/DTreeLearning.py
+++ Curso Python/Tema 3/DTreeLearning.py
@@ -87,7 +87,7 @@ plt.show()
 
 
 from sklearn.ensemble import RandomForestClassifier
-forest = RandomForestClassifier(criterion='gini',n_estimators=25,random_state=1,n_jobs=2)
+forest = RandomForestClassifier(criterion='entropy',n_estimators=25,random_state=1,n_jobs=2)
 forest.fit(X_train, y_train)
 plot_decision_regions(X_combined, y_combined,classifier=forest, test_idx=range(105,150))
 plt.xlabel('petal length [cm]')
