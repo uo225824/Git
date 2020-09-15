f468b85cfc9d16d99a2d34c6b94f0485bc8fd024
uo225824 <christiaapin06-gmail.com>
2020-09-15 02:11:57 +0200

    5

--- Tema 5/KPCA.py
+++ Tema 5/KPCA.py
@@ -187,3 +187,224 @@ plt.show()
 
 
 
+#projecting new data
+
+
+from scipy.spatial.distance import pdist, squareform
+from scipy import exp
+from scipy.linalg import eigh
+import numpy as np
+
+
+def rbf_kernel_pca(X, gamma, n_components):
+    """
+    RBF kernel PCA implementation.
+
+    Parameters
+    ------------
+    X: {NumPy ndarray}, shape = [n_samples, n_features]
+
+    gamma: float
+      Tuning parameter of the RBF kernel
+
+    n_components: int
+      Number of principal components to return
+
+    Returns
+    ------------
+     X_pc: {NumPy ndarray}, shape = [n_samples, k_features]
+       Projected dataset
+
+     lambdas: list
+       Eigenvalues
+
+    """
+    # Calculate pairwise squared Euclidean distances
+    # in the MxN dimensional dataset.
+    sq_dists = pdist(X, 'sqeuclidean')
+
+    # Convert pairwise distances into a square matrix.
+    mat_sq_dists = squareform(sq_dists)
+
+    # Compute the symmetric kernel matrix.
+    K = exp(-gamma * mat_sq_dists)
+
+    # Center the kernel matrix.
+    N = K.shape[0]
+    one_n = np.ones((N, N)) / N
+    K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
+
+    # Obtaining eigenpairs from the centered kernel matrix
+    # numpy.eigh returns them in sorted order
+    eigvals, eigvecs = eigh(K)
+
+    # Collect the top k eigenvectors (projected samples)
+    alphas = np.column_stack((eigvecs[:, -i]
+                              for i in range(1, n_components + 1)))
+    # Collect the corresponding eigenvalues
+    lambdas = [eigvals[-i] for i in range(1, n_components + 1)]
+
+    return alphas, lambdas
+
+
+
+X, y = make_moons(n_samples=100, random_state=123)
+alphas, lambdas = rbf_kernel_pca(X, gamma=15, n_components=1)
+
+
+x_new = X[-1]
+x_new
+x_proj = alphas[-1] # original projection
+x_proj
+
+
+def project_x(x_new, X, gamma, alphas, lambdas):
+    pair_dist = np.array([np.sum((x_new - row)**2) for row in X])
+    k = np.exp(-gamma * pair_dist)
+    return k.dot(alphas / lambdas)
+
+# projection of the "new" datapoint
+x_reproj = project_x(x_new, X, gamma=15, alphas=alphas, lambdas=lambdas)
+x_reproj
+
+
+plt.scatter(alphas[y == 0, 0], np.zeros((50)),
+            color='red', marker='^', alpha=0.5)
+plt.scatter(alphas[y == 1, 0], np.zeros((50)),
+            color='blue', marker='o', alpha=0.5)
+plt.scatter(x_proj, 0, color='black',
+            label='original projection of point X[25]', marker='^', s=100)
+plt.scatter(x_reproj, 0, color='green',
+            label='remapped point X[25]', marker='x', s=500)
+plt.legend(scatterpoints=1)
+
+plt.tight_layout()
+# plt.savefig('./figures/reproject.png', dpi=300)
+plt.show()
+
+
+X, y = make_moons(n_samples=100, random_state=123)
+alphas, lambdas = rbf_kernel_pca(X[:-1, :], gamma=15, n_components=1)
+
+def project_x(x_new, X, gamma, alphas, lambdas):
+    pair_dist = np.array([np.sum((x_new - row)**2) for row in X])
+    k = np.exp(-gamma * pair_dist)
+    return k.dot(alphas / lambdas)
+
+# projection of the "new" datapoint
+x_new = X[-1]
+
+x_reproj = project_x(x_new, X[:-1], gamma=15, alphas=alphas, lambdas=lambdas)
+
+
+plt.scatter(alphas[y[:-1] == 0, 0], np.zeros((50)),
+            color='red', marker='^', alpha=0.5)
+plt.scatter(alphas[y[:-1] == 1, 0], np.zeros((49)),
+            color='blue', marker='o', alpha=0.5)
+plt.scatter(x_reproj, 0, color='green',
+            label='new point [ 100.0,  100.0]', marker='x', s=500)
+plt.legend(scatterpoints=1)
+
+
+plt.scatter(alphas[y[:-1] == 0, 0], np.zeros((50)),
+            color='red', marker='^', alpha=0.5)
+plt.scatter(alphas[y[:-1] == 1, 0], np.zeros((49)),
+            color='blue', marker='o', alpha=0.5)
+plt.scatter(x_proj, 0, color='black',
+            label='some point [1.8713,  0.0093]', marker='^', s=100)
+plt.scatter(x_reproj, 0, color='green',
+            label='new point [ 100.0,  100.0]', marker='x', s=500)
+plt.legend(scatterpoints=1)
+
+plt.tight_layout()
+# plt.savefig('./figures/reproject.png', dpi=300)
+plt.show()
+
+
+#IMPLEMENTATION WITH SCIKIT
+
+
+from sklearn.decomposition import KernelPCA
+
+X, y = make_moons(n_samples=100, random_state=123)
+scikit_kpca = KernelPCA(n_components=2, kernel='rbf', gamma=15)
+X_skernpca = scikit_kpca.fit_transform(X)
+
+plt.scatter(X_skernpca[y == 0, 0], X_skernpca[y == 0, 1],
+            color='red', marker='^', alpha=0.5)
+plt.scatter(X_skernpca[y == 1, 0], X_skernpca[y == 1, 1],
+            color='blue', marker='o', alpha=0.5)
+
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.tight_layout()
+# plt.savefig('./figures/scikit_kpca.png', dpi=300)
+plt.show()
+
+
+#Aplication with SVM
+
+from sklearn.svm import SVC
+from matplotlib.colors import ListedColormap
+
+def versiontuple(v):
+    return tuple(map(int, (v.split("."))))
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.6,
+                    c=cmap(idx),
+                    edgecolor='black',
+                    marker=markers[idx],
+                    label=cl)
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        if not versiontuple(np.__version__) >= versiontuple('1.9.0'):
+            X_test, y_test = X[list(test_idx), :], y[list(test_idx)]
+            warnings.warn('Please update to NumPy 1.9.0 or newer')
+        else:
+            X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    alpha=1.0,
+                    edgecolor='black',
+                    linewidths=1,
+                    marker='o',
+                    s=55, label='test set')
+
+
+svm = SVC(kernel='linear', C=1.0, random_state=1)
+svm.fit(X_skernpca, y)
+
+plot_decision_regions(X_skernpca,y,classifier=svm,test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+
+
