250d97ef545c3d253affc1d0ce90f4b798304cc3
uo225824 <christiaapin06-gmail.com>
2020-09-15 09:26:48 +0200

    5

--- Tema 5/KPCA.py
+++ Tema 5/KPCA.py
@@ -399,7 +399,7 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
 svm = SVC(kernel='linear', C=1.0, random_state=1)
 svm.fit(X_skernpca, y)
 
-plot_decision_regions(X_skernpca,y,classifier=svm,test_idx=range(105, 150))
+plot_decision_regions(X_skernpca,y,classifier=svm,test_idx=None)
 plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
