a41b368fd9b8d9ad8038906ff0474b4cf8a96e36
uo225824 <christiaapin06-gmail.com>
2020-09-24 16:27:37 +0200

    5

--- Tema 14/AdvanzedTF.py
+++ Tema 14/AdvanzedTF.py
@@ -147,3 +147,164 @@ dloss_dw=tape.gradient(loss,w)
 tf.print('dL/dw : ', dloss_dw)
 
 tf.print(2*x * ((w*x + b) - y))
+
+#Simplifying implementations of common architectures via the Keras API
+
+
+model=tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(units=16,activation='relu'))
+model.add(tf.keras.layers.Dense(units=32,activation='relu'))
+model.build(input_shape=(None,4))
+model.summary()
+
+##printing variables of the model
+for v in model.variables:
+    print('{:20s}'.format(v.name),v.trainable, v.shape)
+
+
+model=tf.keras.Sequential()
+model.add(
+    tf.keras.layers.Dense(
+        units=16,
+        activation=tf.keras.activations.relu,
+        kernel_initializer=tf.keras.initializers.glorot_uniform(),
+        bias_initializer=tf.keras.initializers.Constant(2.0)
+    )
+)
+
+model.add(
+    tf.keras.layers.Dense(
+        units=32,
+        activation=tf.keras.activations.sigmoid,
+        kernel_regularizer=tf.keras.regularizers.L1
+    )
+)
+
+model.compile(
+    optimizer=tf.keras.optimizers.SGD(learning_rate=0.01),
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    metrics=[
+        tf.keras.metrics.Accuracy(),
+        tf.keras.metrics.Precision(),
+        tf.keras.metrics.Recall()
+    ]
+)
+
+
+#Solving an XOR classification problem
+
+
+tf.random.set_seed(1)
+np.random.seed(1)
+x = np.random.uniform(low=-1, high=1, size=(200, 2))
+y = np.ones(len(x))
+y[x[:, 0] * x[:, 1]<0] = 0
+
+x_train = x[:100, :]
+y_train = y[:100]
+x_valid = x[100:, :]
+y_valid = y[100:]
+
+fig = plt.figure(figsize=(6, 6))
+plt.plot(x[y==0, 0],
+         x[y==0, 1], 'o', alpha=0.75, markersize=10)
+plt.plot(x[y==1, 0],
+         x[y==1, 1], '<', alpha=0.75, markersize=10)
+plt.xlabel(r'$x_1$', size=15)
+plt.ylabel(r'$x_2$', size=15)
+plt.show()
+
+
+#Using a basic NN
+
+model=tf.keras.Sequential()
+model.add(
+    tf.keras.layers.Dense(
+        units=1,
+        input_shape=(2,),
+        activation='sigmoid'
+    )
+)
+
+model.compile(
+    optimizer=tf.keras.optimizers.SGD(),
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    metrics=[tf.keras.metrics.BinaryAccuracy()]
+)
+
+hist=model.fit(x_train,y_train,validation_data=(x_valid,y_valid),
+               epochs=200,batch_size=2, verbose=0)
+
+from mlxtend.plotting import plot_decision_regions
+
+history = hist.history
+
+fig = plt.figure(figsize=(16, 4))
+ax = fig.add_subplot(1, 3, 1)
+plt.plot(history['loss'], lw=4)
+plt.plot(history['val_loss'], lw=4)
+plt.legend(['Train loss', 'Validation loss'], fontsize=15)
+ax.set_xlabel('Epochs', size=15)
+
+ax = fig.add_subplot(1, 3, 2)
+plt.plot(history['binary_accuracy'], lw=4)
+plt.plot(history['val_binary_accuracy'], lw=4)
+plt.legend(['Train Acc.', 'Validation Acc.'], fontsize=15)
+ax.set_xlabel('Epochs', size=15)
+
+ax = fig.add_subplot(1, 3, 3)
+plot_decision_regions(X=x_valid, y=y_valid.astype(np.integer),
+                      clf=model)
+ax.set_xlabel(r'$x_1$', size=15)
+ax.xaxis.set_label_coords(1, -0.025)
+ax.set_ylabel(r'$x_2$', size=15)
+ax.yaxis.set_label_coords(-0.025, 1)
+plt.show()
+
+
+#A complex model for XOR
+
+tf.random.set_seed(1)
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(units=4, input_shape=(2,), activation='relu'))
+model.add(tf.keras.layers.Dense(units=4, activation='relu'))
+model.add(tf.keras.layers.Dense(units=4, activation='relu'))
+model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+model.summary()
+
+## compile:
+model.compile(optimizer=tf.keras.optimizers.SGD(),
+              loss=tf.keras.losses.BinaryCrossentropy(),
+              metrics=[tf.keras.metrics.BinaryAccuracy()])
+
+## train:
+hist = model.fit(x_train, y_train,
+                 validation_data=(x_valid, y_valid),
+                 epochs=200, batch_size=2, verbose=0)
+
+history = hist.history
+
+
+fig = plt.figure(figsize=(16, 4))
+ax = fig.add_subplot(1, 3, 1)
+plt.plot(history['loss'], lw=4)
+plt.plot(history['val_loss'], lw=4)
+plt.legend(['Train loss', 'Validation loss'], fontsize=15)
+ax.set_xlabel('Epochs', size=15)
+
+ax = fig.add_subplot(1, 3, 2)
+plt.plot(history['binary_accuracy'], lw=4)
+plt.plot(history['val_binary_accuracy'], lw=4)
+plt.legend(['Train Acc.', 'Validation Acc.'], fontsize=15)
+ax.set_xlabel('Epochs', size=15)
+
+ax = fig.add_subplot(1, 3, 3)
+plot_decision_regions(X=x_valid, y=y_valid.astype(np.integer),
+                      clf=model)
+ax.set_xlabel(r'$x_1$', size=15)
+ax.xaxis.set_label_coords(1, -0.025)
+ax.set_ylabel(r'$x_2$', size=15)
+ax.yaxis.set_label_coords(-0.025, 1)
+plt.show()
